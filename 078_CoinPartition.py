303fdd851acb3df7ae0938587d2d3e5442baceee
GalvagniFederico <f.galvagni@cfelettroautomazioni.it>
2023-06-09 21:15:10 +0200

    Finally solved 078 Coin Partition

--- 078_CoinPartition.py
+++ 078_CoinPartition.py
@@ -1,15 +1,102 @@
-def CoinPartitions():
-    result = 0
-    fib = [0,1]
-    print(10000000%1000000)
-    while fib[1]%1000000 !=0:
-        t = fib[1]
-        fib[1] += fib[0]
-        fib[0] = t
-        print(fib[1])
+import math
 
-CoinPartitions()
+def pentagonal(n):
+    result = (n * ((3*n)-1))/2
+    return int(result)
 
+def division(n):
+    res = 0
+    for i in range(n, 1, -1):
+        if n%i == 0:
+            res+=1
+        elif (n%i)%1==0:
+            res+=1
+
+# p(n) = p(n-1) + p(n-2) - p(n-5) - p(n-7)
+# 5 7 12 15 22 sequenza generalizzata pentagonal number ottenuta da (n*((3*n)-1))/2 chiamata in sequenza 0 1 -1 2 -2 3 -3 ....
+def partition():
+    p_mem = {0:1,
+             1:1,
+             2: 2}
+             
+    res = 0
+    n = 3
+
+    pentagonals = {}
+    while p_mem[n-1]%1000000 != 0:
+        # creo tutti i numeri pentagonali fino a n
+        for n_i in range(3,n+1):
+
+            # First I sum p(n-1) + p(n-2) then i subtract p(n-gpns) gp: generalized pentagonal number sequence
+            p_n = p_mem[n_i-1] +p_mem[n_i-2]
+
+            iPentagonal = 2
+
+            while True:
+                if not pentagonals.__contains__(iPentagonal):
+
+                    pentagonals[iPentagonal]= pentagonal(iPentagonal)
+                if n_i - pentagonals[iPentagonal] < 0:
+                    break
+
+
+                if iPentagonal%2==0:
+                    p_n -= p_mem[n_i - pentagonals[iPentagonal]]
+                else:
+                    p_n += p_mem[n_i - pentagonals[iPentagonal]]
+
+                if iPentagonal < 0:
+                    iPentagonal *= -1
+                    iPentagonal += 1
+
+                else:
+                    iPentagonal *= -1
+    
+            p_mem[n_i] = p_n
+            
+        
+        print(str(n) + " - " + str(p_mem[n]))
+        
+        n+=1
+        
+    print(n)
+
+#partition()
+
+def partition2():
+    pentagonals = [1,2]
+    partitions = [1,1]
+       
+    lastpartition = 1
+    n = 2 
+
+    while lastpartition%1000000!=0:
+        lastpartition = 1
+        pentagonals.append(pentagonal(n))
+        pentagonals.append(pentagonal(-n))
+        lastpartition = 0
+
+
+
+        i = 0
+        while n - pentagonals[i] >= 0:
+            diff = n-pentagonals[i]   
+            tr = partitions[diff]
+            if math.floor((i+2)/2) % 2 == 1:    
+                lastpartition += partitions[n-pentagonals[i]]
+            else:
+                lastpartition -= partitions[n-pentagonals[i]]
+            i+=1
+        partitions.append(lastpartition)
+        n+=1
+
+    return n
+            
+
+        
+
+
+print(partition2())
 
 
     # 3 - 3
@@ -17,14 +104,15 @@ CoinPartitions()
     # OO O
     # O O O
 
-    # 4 - 4
+    # 4 - 5
     # OOOO
     # OOO O
     # OO OO
+    # OO O O
     # O O O O
 
     # 5 - 7
-    #     OOOOO
+    # OOOOO
     # OOOO   O
     # OOO   OO
     # OOO   O   O
@@ -45,3 +133,13 @@ CoinPartitions()
     # 00 00 0 0
     # 00 0 0 0 0
     # 0 0 0 0 0 0
+
+    # 7 
+    # OOOOOOO
+    # 0 000000
+    # 00 00000
+    # 000 0000
+    # 0 00000
+    #
+    #
+    #
